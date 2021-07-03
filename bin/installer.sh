5825d41bc95fd8541c75d092ebb6d17f0f90bdd8
Harveen Singh Chadha <30959215+harveenchadha@users.noreply.github.com>
2021-07-04 01:42:04 +0530

    Add files via upload

---
+++ bin/installer.sh
@@ -0,0 +1,22 @@
+sudo apt-get install liblzma-dev libbz2-dev libzstd-dev libsndfile1-dev libopenblas-dev libfftw3-dev libgflags-dev libgoogle-glog-dev
+sudo apt install build-essential cmake libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-test-dev libeigen3-dev zlib1g-dev libbz2-dev 
+sudo apt-get install ffmpeg
+sudo apt-get install sox
+
+cd /usr/local/lib/python3.7/dist-packages
+
+git clone https://github.com/kpu/kenlm.git
+cd kenlm
+mkdir -p build && cd build
+cmake .. 
+make -j 16
+cd ..
+export KENLM_ROOT=$PWD
+export USE_CUDA=0 ## for cpu
+cd ..
+
+
+git clone https://github.com/flashlight/flashlight.git
+cd flashlight/bindings/python
+export USE_MKL=0
+python setup.py install
