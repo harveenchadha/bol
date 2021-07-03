b2effc1bef43aed6f7249296b2fbbb2885edd0eb
Harveen Singh Chadha <30959215+harveenchadha@users.noreply.github.com>
2021-07-03 23:01:47 +0530

    Create Setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bol',
+      version='0.1',
+      description='Speech to Text Library',
+      url='https://github.com/harveenchadha/bol/',
+      author='Harveen Singh Chadha',
+      author_email='harveen54@gmail.com',
+      license='MIT',
+      packages=['bol'],
+      zip_safe=False)
